5ae71d300fe8993c29bd4d40befce326b73eec77
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-08-24 16:50:31 +0300

    using the correct variable names

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let height = 0;
-let excess = 0;
+let total = 0;
+let maxTotal = 100;
+let minTotal = 0;
+let exceedMaxTotal = 0;
+let exceedNormal = 0;
+let color = ['lightblue', 'lightgreen', 'tomato', 'white'];
+[colorLess, colorEqual, colorMore, colorBackgroud] = color;
+let canvasVALUE = [0, 0, 50, 100];
+[canvasX, canvasY, canvasWIDTH, canvasHEIGHT] = canvasVALUE;
 
 let canvas = document.querySelector('.canvas');
 let textPlus = document.querySelector('.text-plus');
@@ -8,57 +15,56 @@ let buttonPlus = document.querySelector('.button-plus');
 let buttonMinus = document.querySelector('.button-minus');
 let msg = document.querySelector('.message');
 
-let ctx1 = canvas.getContext('2d');
-let ctx2 = canvas.getContext('2d');
+let ctxIncrease = canvas.getContext('2d');
+let ctxDecrease = canvas.getContext('2d');
 
 buttonPlus.addEventListener('click', function() {
 
-		if (height < 100) {
-			height += parseInt(textPlus.value) ? parseInt(textPlus.value) : 0;
-			ctx1.fillStyle = 'lightblue';
-			ctx1.fillRect(0,100 - height,50,100);
-			excess = 0;
+		if (total < maxTotal) {
+			total += parseInt(textPlus.value) ? parseInt(textPlus.value) : 0;
+			ctxIncrease.fillStyle = colorLess;
+			ctxIncrease.fillRect(canvasX,maxTotal - total,canvasWIDTH,canvasHEIGHT);
+			exceedMaxTotal = exceedNormal;
 		}
 
-		if (height == 100) {
-			ctx1.fillStyle = "lightgreen";
-			ctx1.fillRect(0,0,50,100);
+		if (total == maxTotal) {
+			ctxIncrease.fillStyle = colorEqual;
+			ctxIncrease.fillRect(canvasX,canvasY,canvasWIDTH,canvasHEIGHT);
 		}
 
-		if (height > 100) {
-			ctx1.fillStyle = "tomato";
-			excess = height - 100;
-			ctx1.fillStyle = "tomato";
-			ctx1.fillRect(0,0,50,100);
+		if (total > maxTotal) {
+			ctxIncrease.fillStyle = colorMore;
+			ctxIncrease.fillRect(canvasX,canvasY,canvasWIDTH,canvasHEIGHT);
+			exceedMaxTotal = total - maxTotal;
 		}
 
-		msg.innerText = `height: ${height}, excess:${excess}`;	
+		msg.innerHTML = `<p>total: ${total}, <span class='exceed'>exceed:${exceedMaxTotal}</span></p>`;	
 		textPlus.value = ``;	
 });
 
 buttonMinus.addEventListener('click', function() {
-	if (height > 0) {
-			height -= parseInt(textMinus.value) ? parseInt(textMinus.value) : 0;
-			excess = 0;
-			ctx2.fillStyle = 'white';
-			ctx2.fillRect(0,0,50,100 - height);		
-			ctx1.fillStyle = 'lightblue';
-			ctx1.fillRect(0,100 - height,50,100);	
+	if (total > minTotal) {
+			total -= parseInt(textMinus.value) ? parseInt(textMinus.value) : 0;
+			ctxDecrease.fillStyle = colorBackgroud;
+			ctxDecrease.fillRect(canvasX,canvasY,canvasWIDTH,maxTotal - total);		
+			ctxIncrease.fillStyle = colorLess;
+			ctxIncrease.fillRect(canvasX,maxTotal - total,canvasWIDTH,canvasHEIGHT);	
+			exceedMaxTotal = exceedNormal;
 
-		if (height > 100) {
-			excess = height - 100;		
-			ctx1.fillStyle = "tomato";
-			ctx1.fillRect(0,0,50,100);
+		if (total > maxTotal) {
+			ctxIncrease.fillStyle = colorMore;
+			ctxIncrease.fillRect(canvasX,canvasY,canvasWIDTH,canvasHEIGHT);
+			exceedMaxTotal = total - maxTotal;		
 		} 
 	}
 
-	if (height == 100) {
-		ctx1.fillStyle = "lightgreen";
-		ctx1.fillRect(0,0,50,100);
-		excess = height - 100;
+	if (total == maxTotal) {
+		ctxIncrease.fillStyle = colorEqual;
+		ctxIncrease.fillRect(canvasX,canvasY,canvasWIDTH,canvasHEIGHT);
+		exceedMaxTotal = total - maxTotal;
 	}
 
-		msg.innerHTML = `<p>height: ${height}, <span class='excess'>excess:${excess}</span></p>`;	
+		msg.innerHTML = `<p>total: ${total}, <span class='exceed'>exceed:${exceedMaxTotal}</span></p>`;	
 		textMinus.value = ``;	
 });
 
